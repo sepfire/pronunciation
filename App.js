6dafa55d4dc6e82ce94c1932672a5b00b2a107bc
matti pitkänen <sepfire.m@gmail.com>
2024-02-07 09:20:33 +0200

    Pronunciation exercise

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [thingToSay, setThingToSay] = useState('');
+  const speak = () => {
+
+    Speech.speak(thingToSay);
+    setThingToSay(''); // clear the input field after speaking
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input}
+        placeholder='Write text here'
+        onChangeText={text => setThingToSay(text)}
+        value={thingToSay} />
+
+      <Button title="Press to hear pronunciation" onPress={speak} />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
   },
 });
